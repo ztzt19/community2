d458848c436ec963b4b68699b69f19caa74f836b
ztzt19 <1120107300@qq.com>
2021-03-04 14:42:49 +0800

    h2 insert

---
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package life.majiang.community.mapper;
+
+import life.majiang.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author zt
+ * @create 2021-03-04 11:00
+ */
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
