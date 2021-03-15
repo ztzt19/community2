3f0e3acf36c0385db09106722a3a0661514bc84d
ztzt19 <1120107300@qq.com>
2021-03-15 20:19:02 +0800

    P41

--- src/main/java/life/majiang/community/dto/QuestionDTO.java
+++ src/main/java/life/majiang/community/dto/QuestionDTO.java
@@ -9,13 +9,11 @@ import lombok.Data;
  */
 @Data
 public class QuestionDTO {
-//    private Integer id;
     private Long id;
     private String title;
     private String description;
     private Long gmtCreate;
     private Long gmtModified;
-//    private Integer creator;
     private Long creator;
     private Integer commentCount;
     private Integer viewCount;
--- src/main/java/life/majiang/community/service/CommentService.java
+++ src/main/java/life/majiang/community/service/CommentService.java
@@ -11,6 +11,7 @@ import life.majiang.community.model.Comment;
 import life.majiang.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author zt
@@ -27,6 +28,7 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
