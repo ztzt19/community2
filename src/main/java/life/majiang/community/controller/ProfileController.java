e67313719015963de49f33121a94d623fc12e05f
ztzt19 <1120107300@qq.com>
2021-03-19 15:37:34 +0800

    P51 add markdown

--- src/main/java/life/majiang/community/controller/ProfileController.java
+++ src/main/java/life/majiang/community/controller/ProfileController.java
@@ -52,7 +52,7 @@ public class ProfileController {
             Long unreadCount = notificationService.unreadCount(user.getId());
             model.addAttribute("section","replies");
             model.addAttribute("pagination",paginationDTO);
-            model.addAttribute("unreadCount",unreadCount);
+//            model.addAttribute("unreadCount",unreadCount);
             model.addAttribute("sectionName","最新回复");
         }
         return "profile";
