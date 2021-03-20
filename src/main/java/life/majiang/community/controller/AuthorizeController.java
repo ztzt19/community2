bd4af48c8bd0203c3dcbddc76ac0a0d94578d6ce
ztzt19 <1120107300@qq.com>
2021-03-20 21:56:00 +0800

    P61 追加日志

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -6,6 +6,7 @@ import life.majiang.community.mapper.UserMapper;
 import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
 import life.majiang.community.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -22,6 +23,7 @@ import java.util.UUID;
  * @create 2021-03-03 13:04
  */
 @Controller
+@Slf4j
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
@@ -67,6 +69,7 @@ public class AuthorizeController {
 //            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         }else {
+            log.error("callback get github error,{}",githubUser);
             //登录失败，重新登录
             return "redirect:/";
         }
