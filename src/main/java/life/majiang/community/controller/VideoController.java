ed827196800ebad5670a2220886fc283c1e4f68f
ztzt19 <1120107300@qq.com>
2021-04-03 17:04:19 +0800

    添加视频详情页面

--- src/main/java/life/majiang/community/controller/VideoController.java
+++ src/main/java/life/majiang/community/controller/VideoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -17,5 +18,13 @@ public class VideoController {
     @GetMapping("/videos")
     public String index(){
         return "videos";
+    }
+    @GetMapping("/video")
+    public String video(@RequestParam(name = "aid") String aid,
+                        @RequestParam(name = "page", defaultValue = "1") String page,
+                        Model model){
+        model.addAttribute("aid",aid);
+        model.addAttribute("page",page);
+        return "video";
     }
 }
