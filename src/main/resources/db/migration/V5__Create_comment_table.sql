7a9a4fbcf7e7687b6ff036a05bba949578c1b380
ztzt19 <1120107300@qq.com>
2021-04-10 14:24:01 +0800

    修复问题详情页面-3

--- src/main/resources/db/migration/V5__Create_comment_table.sql
+++ src/main/resources/db/migration/V5__Create_comment_table.sql
@@ -7,7 +7,7 @@ create table comment
 	gmt_create bigint not null,
 	gmt_modified bigint not null,
 	like_count bigint default 0,
--- 	comment_count bigint default 0,
+	comment_count bigint default 0,
 	content_count bigint default 0
 );
 
